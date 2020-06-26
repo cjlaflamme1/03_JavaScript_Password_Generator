a881751eb7fbb326c1d776c3fbc18b72d5a57b3c
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-06-26 10:25:35 -0400

    Defined Character variables

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const generateBtn = document.querySelector("#generate");
 const characterAmountSlider = document.getElementById('numberOfCharacterInputSlider');
 const characterAmountNumber = document.getElementById('numberOfCharacterInputDigit');
 
+const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz".split('');
+const upperCaseChar = "ABCDEFGHIJKLMNOPQRESTUVWXYZ".split('');
+const numberChar = "0123456789".split('');
+const specialChar = "`~!@#$%^&*()-_=+[]\|}{',./<>?".split('');
+
+
 
 characterAmountSlider.addEventListener('input', syncCharacterAmount);
 characterAmountNumber.addEventListener('input', syncCharacterAmount);
